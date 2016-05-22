35875453b6e9667f6c9623206b3ed9482d775850
TakedaLeyasu <c.frauendorf2000@web.de>
2016-05-22 10:06:31 +0200

    1.Version
    1.Version

---
+++ app/src/main/java/com/wordpress/httpseliminatorgang/mymedicine/QRCodeActivity.java
@@ -0,0 +1,18 @@
+package com.wordpress.httpseliminatorgang.mymedicine;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Cedric on 22.05.2016.
+ */
+public class QRCodeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.acticity_qr_code);
+
+
+    }
+}
