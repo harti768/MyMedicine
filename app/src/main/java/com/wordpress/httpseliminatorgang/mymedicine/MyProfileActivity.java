22bf5c2213b6fa28bfa0901089f44bb30ae5dadc
TakedaLeyasu <c.frauendorf2000@web.de>
2016-06-07 23:25:40 +0200

    Mein Profil Activity hinzugefügt

---
+++ app/src/main/java/com/wordpress/httpseliminatorgang/mymedicine/MyProfileActivity.java
@@ -0,0 +1,17 @@
+package com.wordpress.httpseliminatorgang.mymedicine;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Cedric on 07.06.2016.
+ */
+public class MyProfileActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_profile);
+
+
+    }
+}
